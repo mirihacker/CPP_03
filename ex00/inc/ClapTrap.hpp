0f355b13df097ebce79e6ca977837bf64f762e32
Siria <siria_miren@icloud.com>
2025-04-02 14:09:33 +0200

    ex00 on the works

---
+++ ex00/inc/ClapTrap.hpp
@@ -0,0 +1,34 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#include <iostream>
+#include <limits>
+
+class ClapTrap
+{
+		private:
+			std::string _name;
+			unsigned int _hitPoints;
+			unsigned int _energyPoints;
+			unsigned int _attackDamage;
+			bool _checkPoints(void);
+		
+		public:
+			ClapTrap(void);
+			ClapTrap(std::string name);
+			ClapTrap(ClapTrap const &other);
+			ClapTrap& operator=(ClapTrap const &other);
+			~ClapTrap(void);
+			
+			void attack(const std::string &target);
+			void takeDamage(unsigned int amount);
+			void beRepaired(unsigned int amount);
+			
+			unsigned int getHitPoints(void);
+			unsigned int getEnergyPoints(void);
+			unsigned int getAttackDamage(void);
+};
+
+std::ostream &operator<<(std::ostream &out, ClapTrap &clap);
+
+#endif
---
+++ ex00/src/ClapTrap.cpp
@@ -0,0 +1,107 @@
+#include "../inc/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : _name("CT"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap constructor is called. Initialized as " << _name << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap constructor is called. Initialized as " <<_name << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+    _name = other._name;
+    _hitPoints = other._hitPoints;
+    _energyPoints = other._energyPoints;
+    _attackDamage = other._attackDamage;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other)
+{
+    if (this != &other)
+    {
+        _name = other._name;
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage; 
+    }
+    return (*this);
+}
+
+ClapTrap::~ClapTrap(void)
+{
+    std::cout << "ClapTrap " << _name << " is destroyed!" << std::endl;
+}
+
+bool ClapTrap::_checkPoints(void)
+{
+    if (!_energyPoints)
+    {
+        std::cout << "No energy points!" << std::endl;
+        return false;
+    }
+    if (!_hitPoints)
+    {
+        std::cout << "Sorry no hit points!" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+    if (!_checkPoints)
+        return;
+    _energyPoints--;
+    std::cout << "ClapTrap " << _name << " attacks " << target << ", causing ";
+    std::cout << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    std::cout << "ClapTrap " << _name << " is attacked ";
+    std::cout << amount << " points of damage!" << std::endl;
+    if (_hitPoints < amount)
+        _hitPoints = 0;
+    else
+        _hitPoints -= amount;
+    std::cout << "Current hit point amount is " << _hitPoints << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (!_checkPoints())
+        return;
+    _energyPoints--;
+    std::cout << "Hit points are repaired by " << amount << std::endl;
+    if (std::numeric_limits<unsigned int>::max() - amount <_hitPoints)
+        _hitPoints = std::numeric_limits<unsigned int>::max();
+    else
+        _hitPoints += amount;
+    std::cout << "Current hit points status " << _hitPoints << std::endl;
+}
+
+unsigned int ClapTrap::getHitPoints(void)
+{
+    return (_hitPoints);
+}
+
+unsigned int ClapTrap::getEnergyPoints(void)
+{
+    return (_energyPoints);
+}
+
+unsigned int ClapTrap::getAttackDamage(void)
+{
+    return (_attackDamage);
+}
+
+std::ostream &operator<<(std::ostream &out, ClapTrap &clap)
+{
+    out << "Hit Points: " << clap.getHitPoints() << ", ";
+    out << "Energy Points: " << clap.getEnergyPoints() << ", ";
+    out << "Attack Damage: " << clap.getAttackDamage() << ", ";
+    return (out);
+}
